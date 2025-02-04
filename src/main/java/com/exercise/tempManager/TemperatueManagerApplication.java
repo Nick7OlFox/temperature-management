36563295e1ce79c2aa5aa32fd480b78b1e614d08
Bruno Faria Almeida <nicholas7fox@gmail.com>
2025-02-04 23:20:21 +0000

    Add initial files

---
+++ src/main/java/com/exercise/tempManager/TemperatueManagerApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.tempManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatueManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatueManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/tempManager/TemperatueManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.tempManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatueManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
