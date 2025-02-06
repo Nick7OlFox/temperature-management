ddcd6b132c3b4e6ff5db3ee95ae83a588590bc89
Bruno Faria Almeida <nicholas7fox@gmail.com>
2025-02-06 17:39:36 +0000

    Removed column

--- data/tables/CREATE_TABLE_DEVICE.sql
+++ data/tables/CREATE_TABLE_DEVICE.sql
@@ -2,6 +2,5 @@ CREATE TABLE devices
 (
     device_name VARCHAR(255) NOT NULL UNIQUE COMMENT 'Name of the device',
     location VARCHAR(255) COMMENT 'Location of the device',
-    end_date TIMESTAMP COMMENT 'Time stamp that if not null indicates that the entry is archived',
     PRIMARY KEY(device_name)
 ) COMMENT = 'Table to store recognised devices';
--- src/main/java/com/exercise/tempManager/dto/Device.java
+++ src/main/java/com/exercise/tempManager/dto/Device.java
@@ -1,6 +1,7 @@
 package com.exercise.tempManager.dto;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,7 @@ import java.sql.Timestamp;
 /**
  * Class to represent an IoT Device
  */
+@Entity
 @Data
 @Builder
 @AllArgsConstructor
@@ -26,7 +28,4 @@ public class Device {
 
     @Column(name = "location")
     private String location;
-
-    @Column(name = "end_date")
-    private Timestamp endDate;
 }
