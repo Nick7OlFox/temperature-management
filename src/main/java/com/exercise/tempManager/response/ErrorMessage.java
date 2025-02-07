7ce7215f0370642cf6d2288a697f9161a279e644
Bruno Faria Almeida <nicholas7fox@gmail.com>
2025-02-07 10:34:44 +0000

    Added Response Objects

---
+++ src/main/java/com/exercise/tempManager/response/ErrorMessage.java
@@ -0,0 +1,33 @@
+package com.exercise.tempManager.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+    private int statusCode;
+    private Date timestamp;
+    private String message;
+    private String description;
+
+    /**
+     * Constructor to be used when creating the ErrorMessage through the @RestControllerExceptionHandler
+     * @param statusCode The HTTP status code
+     * @param ex The exception that was caught
+     * @param request // TODO Complete description
+     */
+    public ErrorMessage(int statusCode, Exception ex, WebRequest request){
+        this.statusCode = statusCode;
+        this.timestamp = new Date();
+        this.message = ex.getMessage();
+        this.description = request.getDescription(false);
+    }
+}
